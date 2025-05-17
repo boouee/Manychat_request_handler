8b75e84ab71d8134dcbac668855b7a8ab72ea73f
boouee <104202645+boouee@users.noreply.github.com>
2025-05-17 23:25:08 +0500

    Update main.py

--- tgbot/main.py
+++ tgbot/main.py
@@ -1,4 +1,5 @@
 import asyncio
+import asyncpg
 from aiogram import Bot, Dispatcher, Router
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
@@ -6,9 +7,15 @@ from aiogram.types import Message
 #token="Your token was replaced with a new one. You can use this token to access HTTP API:
 token ='7600730617:AAFsVcwR2GI23MnsturSya50ajaF1IT7lb4'
 webhook_url = 'https://simple-tg-bot-aiogram.vercel.app'
+connection_string = 'postgresql://neondb_owner:npg_rzqOTvaJiP01@ep-frosty-morning-a2z2rgqi-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
 router = Router()
 @router.message(CommandStart())
 async def process_start_command(message: Message):
+    pool = await asyncpg.create_pool(connection_string)
+    async with pool.acquire() as conn:
+    # Execute a statement to create a new table.
+        await conn.execute('INSERT INTO users(' + message.from_user.id + ')')
+    await pool.close()
     await message.answer('/start')
 class TGBot:
     def __init__(self, router: Router) -> None:
