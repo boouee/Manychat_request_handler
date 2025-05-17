58c9c7be54848e3bd11673a5a145fd9023e6da94
boouee <104202645+boouee@users.noreply.github.com>
2025-05-17 17:52:30 +0500

    Update main.py

--- tgbot/main.py
+++ tgbot/main.py
@@ -1,29 +1,29 @@
 import asyncio
 from aiogram import Bot, Dispatcher, Router
+from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
 #from tgbot.handlers import router
-
-router = Router(name=__name__)
-@router.message()
-async def message_handler(message: Message) -> Any:
-    await message.answer('Hello from my router!')
+#token="Your token was replaced with a new one. You can use this token to access HTTP API:
+token ='7600730617:AAFsVcwR2GI23MnsturSya50ajaF1IT7lb4'
+webhook_url = 'https://simple-tg-bot-aiogram.vercel.app'
+router = Router()
+@router.message(CommandStart())
+async def process_start_command(message: Message):
+    await message.answer('/start')
 class TGBot:
     def __init__(self, router: Router) -> None:
-       token = config('TOKEN')
+       #token = config('TOKEN')
        self.bot = Bot(token)
        self.dp = Dispatcher()
        self.dp.include_router(router)
-       if not config('DEBUG', default=False):
-           loop = asyncio.get_event_loop()
-           loop.run_until_complete(self.set_commands())
-           loop.run_until_complete(self.set_webhook())
+       
 
     async def update_bot(self, update: dict) -> None:
         await self.dp.feed_raw_update(self.bot, update)
         await self.bot.session.close()
 
     async def set_webhook(self):
-        webhook_url = config('WEBHOOK_URL')
+        #webhook_url = config('WEBHOOK_U
         # WEBHOOK_URL = адрес сайта/api/bot
         await self.bot.set_webhook(webhook_url)
         await self.bot.session.close()
