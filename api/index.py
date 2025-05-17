c28d36865bb8fd11ee00fa3c5f8a75abc739d969
boouee <104202645+boouee@users.noreply.github.com>
2025-05-17 19:46:23 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -6,5 +6,6 @@ app = FastAPI()
 @app.post('/api/bot')
 async def tgbot_webhook_route(request: Request):
     update_dict = await request.json()
+    print(update_dict)
     await tgbot.update_bot(update_dict)
     return ''
