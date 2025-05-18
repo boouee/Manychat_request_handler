08f2a3bd2502dd714341f1f2518c23353a96c28f
boouee <104202645+boouee@users.noreply.github.com>
2025-05-18 15:02:48 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -16,8 +16,8 @@ async def tgbot_webhook_route(request: Request):
 async def send_message(request: Request):
     body = await request.body()
     print(request, body)
-    update_dict = await request.json()
-    print(update_dict)
+    #update_dict = await request.json()
+    #print(update_dict)
     await tgbot.send_message('A message sent')
     return "post accepted"
 
