dd43c63a671798ad429fa6838dac9daa33f7c687
boouee <104202645+boouee@users.noreply.github.com>
2025-06-10 10:01:16 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,5 +1,16 @@
 from urllib.parse import unquote
+import re
 
 def main(request):
-  request = unquote(request)
+  request = unquote(request).split()
+  connector = re.search('\[connector_id\]=(.+?)&', request).group(1)
+  line = re.search('\[line_id\]=(.+?)&', request).group(1)
+  chat = re.search('\[line_id\]=(.+?)&', request).group(1)
+  user = re.search('\[line_id\]=(.+?)&', request).group(1)
+  code = line + "|" + chat
   
+    
+  
+def find(array, term):
+  for i in array:
+    if 
