3b85b95b8b3d6f2ebb406390d16d3b9fd0c2f3bf
boouee <104202645+boouee@users.noreply.github.com>
2025-06-11 07:55:13 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,23 +4,14 @@ from urllib.parse import unquote, urlparse
 
 app = FastAPI()
 
-@app.post('/api/bot')
-async def tgbot_webhook_route(request: Request):
-    body = await request.body()
-    print(request, body)
-    update_dict = await request.json()
-    print(update_dict)
-    await tgbot.update_bot(update_dict)
-    return ''
-
-@app.post('/api/message', status_code='500')
+@app.post('/api/message')
 async def send_message(request: Request):
     body = await request.body()
     print(request, unquote(body.decode()))
     print(urlparse(body.decode()))
     #update_dict = await request.json()
     #print(update_dict)
-    await tgbot.send_message('A message sent')
+    #await tgbot.send_message('A message sent')
     return "post accepted"
 
 @app.get('/api/message')
