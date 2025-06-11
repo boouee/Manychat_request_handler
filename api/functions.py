d12379a24152bd370e97a22721c5e045bbaf1f52
boouee <104202645+boouee@users.noreply.github.com>
2025-06-11 14:51:29 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -1,6 +1,10 @@
 from urllib.parse import unquote
 import httpx
 import re
+import asyncio
+import asyncpg
+
+connection_string = 'postgresql://neondb_owner:npg_rzqOTvaJiP01@ep-frosty-morning-a2z2rgqi-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
 
 def chat_code(request):
   data = {}
@@ -18,7 +22,18 @@ async def chat_id(code):
     response = await client.post('https://b24-dqlsji.bitrix24.ru/rest/1/s8xdt6lup9f63cj2/imopenlines.session.open', data=data)
     return response.json()
     
-  
+async def record_time(chat):
+    pool = await asyncpg.create_pool(connection_string)
+    statement = """
+      INSERT INTO chats (id, time)
+      VALUES ({chat}, {timestamp})
+      ON CONFLICT (id)
+      DO NOTHING | DO UPDATE SET id = {chat}, time = {timestamp};
+      """
+    async with pool.acquire() as conn:
+    # Execute a statement to create a new table.
+        await conn.execute("INSERT INTO users (\"user\") VALUES('" + str(message.from_user.id) + "')")
+    await pool.close()  
 #def find(array, term):
   #for i in array:
     #if 
