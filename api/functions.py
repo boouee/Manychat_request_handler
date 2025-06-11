79af414732ce3a75d292d1fbfa336e9e5d4d2967
boouee <104202645+boouee@users.noreply.github.com>
2025-06-11 09:20:36 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -1,4 +1,5 @@
 from urllib.parse import unquote
+import httpx
 import re
 
 def chat_code(request):
@@ -10,7 +11,12 @@ def chat_code(request):
   data['user'] = re.search('data\[DATA\]\[connector\]\[user_id\]=(.+?)&', request).group(1)
   code = '|'.join(data.values())
   return code
-  
+
+def chat_id(code):
+  async with httpx.AsyncClient() as client:
+    data = {"USER_CODE": code}
+    response = await client.post('https://b24-dqlsji.bitrix24.ru/rest/1/18zmjgd33yujg5bx/imopenlines.session.open', data=data)
+    return response.json()
     
   
 #def find(array, term):
