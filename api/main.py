11450fff27f2a18717176cfd1743b4ac7a8be4d9
boouee <104202645+boouee@users.noreply.github.com>
2025-06-11 08:45:38 +0500

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,17 +1,18 @@
 from urllib.parse import unquote
 import re
 
-def main(request):
+def chat_code(request):
   data = {}
   request = unquote(request).split()
   data['connector'] = re.search('\[connector_id\]=(.+?)&', request).group(1)
   data['line'] = re.search('\[line_id\]=(.+?)&', request).group(1)
-  data['chat'] = re.search('\[line_id\]=(.+?)&', request).group(1)
-  data['user'] = re.search('\[line_id\]=(.+?)&', request).group(1)
-  code = data.join('|') 
+  data['chat'] = re.search('\[chat_id\]=(.+?)&', request).group(1)
+  data['user'] = re.search('data\[DATA\]\[connector\]\[user_id\]=(.+?)&', request).group(1)
+  code = '|'.join(data.values())
+  return code
   
     
   
-def find(array, term):
-  for i in array:
-    if 
+#def find(array, term):
+  #for i in array:
+    #if 
