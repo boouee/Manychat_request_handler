65ed0e8371d363f8b44d2b8d61cdabda3684bf23
boouee <104202645+boouee@users.noreply.github.com>
2025-06-23 00:17:39 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -15,7 +15,7 @@ pipeline = os.getenv("pipeline")
 status = os.getenv("source_status")
 target = os.getenv("target_status")
 #connection_string = 'postgresql://neondb_owner:npg_rzqOTvaJiP01@ep-frosty-morning-a2z2rgqi-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
-headers = {"Authentification": f"Bearer {key}"}
+headers = {"Authorization": f"Bearer {key}"}
     
 async def update_leads():
     url = os.getenv("url")
