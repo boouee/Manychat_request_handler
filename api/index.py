d2bc23b2e222a3afafddbd8194b69d07cd46060f
boouee <104202645+boouee@users.noreply.github.com>
2025-06-22 23:48:05 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,38 +1,13 @@
 from fastapi import FastAPI, Request
-from tgbot.main import tgbot
-from api.functions import chat_code, chat_id
-from urllib.parse import unquote, urlparse
+from api.functions import update_leads
+#from urllib.parse import unquote, urlparse
 
 app = FastAPI()
 
-@app.post('/api/bot')
-async def tgbot_webhook_route(request: Request):
-    body = await request.body()
-    print(request, body)
-    update_dict = await request.json()
-    print(update_dict)
-    await tgbot.update_bot(update_dict)
-    return ''
-
-@app.post('/api/message')
-async def send_message(request: Request):
-    body = await request.body()
-    print(request, unquote(body.decode()))
-    print(urlparse(body.decode()))
+@app.post('/api/update')
+async def update(request: Request):
     try:
-        print(chat_code(body.decode()))
+        await update_leads()
     except Exception as e:
-        print(e)
+        print("Exception: ", e)
 
-    code = chat_code(body.decode())
-    id = await chat_id(code)
-    print(id)
-    #update_dict = await request.json()
-    #print(update_dict)
-    await tgbot.send_message('A message sent')
-    return "post accepted"
-
-@app.get('/api/message')
-async def send_message(request: Request):
-    #await tgbot.send_message('A message sent')
-    return "hello"
