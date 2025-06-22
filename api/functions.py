42ef01835f9cb314f431437df6649f26e8b4fcc1
boouee <104202645+boouee@users.noreply.github.com>
2025-06-23 00:47:12 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -47,8 +47,8 @@ async def change_status(lead):
     url = os.getenv("url")
     url = f"{url}leads/{lead}"
     async with httpx.AsyncClient() as client:
-        data = {"status_id": 123}#int(target)}
+        data = {"status_id": int(target)}
         print(data)
-        response = await client.patch(url, data=data,headers=headers)
+        response = await client.patch(url, json=data,headers=headers)
         json = response.json()   
         print("response: ", json)
