657f6c376b90911db9c5edce79c07a4c3fb5dee9
boouee <104202645+boouee@users.noreply.github.com>
2025-06-22 23:43:17 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -15,17 +15,18 @@ pipeline = os.getenv("pipeline")
 status = os.getenv("source_status")
 target = os.getenv("target_status")
 #connection_string = 'postgresql://neondb_owner:npg_rzqOTvaJiP01@ep-frosty-morning-a2z2rgqi-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
-
+headers = {"Authentification": f"Bearer {key}"}
+    
 async def update_leads():
     hour = time.gmtime().tm_hour + 3
     if hour > 8 and hour < 18:
-        lead = await get_lead
+        lead = await get_lead()
         await change_status(lead)
         
 async def get_lead():
     url = f"{url}leads?filter[statuses][0][pipeline_id]={pipeline}&filter[statuses][0][status_id]={status}"
     async with httpx.AsyncClient() as client:
-        response = await client.get(url)
+        response = await client.get(url,headers=headers)
         json = response.json()
         lead = json["_embedded"]["leads"][0]["id"]
         return lead
@@ -34,5 +35,5 @@ async def change_status(lead):
     url = f"{url}leads/{lead}"
     async with httpx.AsyncClient() as client:
         data = {"status_id": target}
-        response = await client.patch(url, data=data)
+        response = await client.patch(url, data=data,headers=headers)
         json = response.json()    
