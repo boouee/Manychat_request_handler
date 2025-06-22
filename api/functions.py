852f9f48274e5bdede38328ce941f8a0e26efc1f
boouee <104202645+boouee@users.noreply.github.com>
2025-06-22 23:38:32 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -9,11 +9,19 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=".env")
-api = os.getenv("api")
+url = os.getenv("url")
+key = os.getenv("url")
 pipeline = os.getenv("pipeline")
-status = os.getenv("status")
+status = os.getenv("source_status")
+target = os.getenv("target_status")
 #connection_string = 'postgresql://neondb_owner:npg_rzqOTvaJiP01@ep-frosty-morning-a2z2rgqi-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
 
+async def update_leads():
+    hour = time.gmtime().tm_hour + 3
+    if hour > 8 and hour < 18:
+        lead = await get_lead
+        await change_status(lead)
+        
 async def get_lead():
     url = f"{url}leads?filter[statuses][0][pipeline_id]={pipeline}&filter[statuses][0][status_id]={status}"
     async with httpx.AsyncClient() as client:
