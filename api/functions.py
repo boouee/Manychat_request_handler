dc7905d996e7fa42eb8094e34a188deee570e0af
boouee <104202645+boouee@users.noreply.github.com>
2025-06-23 23:13:31 +0500

    Update functions.py

--- api/functions.py
+++ api/functions.py
@@ -6,6 +6,7 @@ import asyncpg
 import time
 import os
 #import redis
+from datetime import date
 from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=".env")
@@ -17,7 +18,7 @@ target = os.getenv("target_status")
 #connection_string = 'postgresql://neondb_owner:npg_rzqOTvaJiP01@ep-frosty-morning-a2z2rgqi-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
 headers = {"Authorization": f"Bearer {key}"}
     
-async def update_leads():
+async def update_leads(days):
     url = os.getenv("url")
     key = os.getenv("key")
     pipeline = os.getenv("pipeline")
@@ -25,9 +26,10 @@ async def update_leads():
     target = os.getenv("target_status")
     #connection_string = 'postgresql://neondb_owner:npg_rzqOTvaJiP01@ep-frosty-morning-a2z2rgqi-pooler.eu-central-1.aws.neon.tech/neondb?sslmode=require'
     headers = {"Authentification": f"Bearer {key}", "Content-Type":"application/json"}
-    
+    day = str(date.today().isoweekday())
+    print('day: ', day)
     hour = time.gmtime().tm_hour + 3
-    if hour > 0 and hour < 30:
+    if hour > 9 and hour < 30 and day in days:
         lead = await get_lead()
         await change_status(lead)
         
