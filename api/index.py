cc5dc09e367054b248f43b852397d0363d8b57fc
boouee <104202645+boouee@users.noreply.github.com>
2025-06-25 22:14:53 +0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -5,7 +5,7 @@ from api.functions import request_handler
 app = FastAPI()
 
 @app.get('/api/send')
-async def update(request: Request, id: str = 'none', tg_username: str):
+async def update(request: Request, id: str = 'none', tg_username: str = ""):
     print(id)
     if id != 'none':
         await request_handler(id)
